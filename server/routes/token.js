43bf758aeeacb56d0842dadab402c41d26c294c1
Yehonatan Calinsky <yonikal56@gmail.com>
2023-05-30 13:18:43 +0300

    Create db and collection, create requests and logic and handle all api requests for users

--- server/routes/token.js
+++ server/routes/token.js
@@ -2,8 +2,6 @@ const express = require('express');
 var router = express.Router();
 const userController = require('../controllers/user');
 
-router.route('/').post(userController.createUser);
-
-router.route('/:username').get(userController.isLoggedIn ,userController.getUserByUsername);
+router.route('/').post(userController.getToken);
 
 module.exports = router;
